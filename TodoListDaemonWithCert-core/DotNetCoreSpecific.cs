cf8cd21e95d7b070604117b7c3a71e7208e88da4
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-06-14 15:46:44 +0200

    Adding a .NET Core project. The .NET core project uses the same source files as the TodoListDaemonWithCert project, plus an additional file (DotNetCoreSpecifics) which re-implements some of the methods / classes (for instance configuraiton) which are not available in .NET Core.

---
+++ TodoListDaemonWithCert-core/DotNetCoreSpecific.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using TodoListDaemonWithCert;
+
+namespace TodoListDaemonWithCert
+{
+    /// <summary>
+    /// Provides Close() for X509Store.
+    /// </summary>
+    public static class Extension
+    {
+        public static void Close(this X509Store store)
+        {
+            store.Dispose();
+        }
+    }
+
+    public class ConfigurationAdapter
+    {
+        public string this[string config]
+        {
+            get
+            {
+                string value = null;
+                var elements = xmlDocument.GetElementsByTagName("add").OfType<XmlElement>();
+                XmlElement element = elements.FirstOrDefault(e => e.Attributes.OfType<XmlAttribute>().Any(a => a.Name == "key" && a.Value == config));
+                if (element != null)
+                {
+                    value = element.Attributes.OfType<XmlAttribute>().FirstOrDefault(a => a.Name == "value").Value;
+                }
+                return value;
+            }
+        }
+
+        readonly XmlDocument xmlDocument;
+
+        public ConfigurationAdapter(string configPath)
+        {
+            xmlDocument = new XmlDocument();
+            xmlDocument.Load(File.OpenText(configPath));
+        }
+    }
+}
+
+/// <summary>
+/// Provides the Configuration manager for .NET Core
+/// </summary>
+namespace System.Configuration
+{
+    public static class ConfigurationManager
+    {
+        public static ConfigurationAdapter AppSettings
+        {
+            get
+            {
+                if (configurationProvider == null)
+                {
+                    configurationProvider = new ConfigurationAdapter(@"..\TodoListDaemonWithCert\App.config");
+                }
+                return configurationProvider;
+            }
+        }
+
+        static ConfigurationAdapter configurationProvider;
+    }
+}
