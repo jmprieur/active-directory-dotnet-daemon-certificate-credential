4b9d5f00c9ecb0f6709ede41f31efd4b62e51e9f
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-06-14 15:54:37 +0200

    Improving the implementation of the DotNetCoreSpecific

--- TodoListDaemonWithCert-core/DotNetCoreSpecific.cs
+++ TodoListDaemonWithCert-core/DotNetCoreSpecific.cs
@@ -4,48 +4,9 @@ using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 using TodoListDaemonWithCert;
 
-namespace TodoListDaemonWithCert
-{
-    /// <summary>
-    /// Provides Close() for X509Store.
-    /// </summary>
-    public static class Extension
-    {
-        public static void Close(this X509Store store)
-        {
-            store.Dispose();
-        }
-    }
-
-    public class ConfigurationAdapter
-    {
-        public string this[string config]
-        {
-            get
-            {
-                string value = null;
-                var elements = xmlDocument.GetElementsByTagName("add").OfType<XmlElement>();
-                XmlElement element = elements.FirstOrDefault(e => e.Attributes.OfType<XmlAttribute>().Any(a => a.Name == "key" && a.Value == config));
-                if (element != null)
-                {
-                    value = element.Attributes.OfType<XmlAttribute>().FirstOrDefault(a => a.Name == "value").Value;
-                }
-                return value;
-            }
-        }
-
-        readonly XmlDocument xmlDocument;
-
-        public ConfigurationAdapter(string configPath)
-        {
-            xmlDocument = new XmlDocument();
-            xmlDocument.Load(File.OpenText(configPath));
-        }
-    }
-}
 
 /// <summary>
-/// Provides the Configuration manager for .NET Core
+/// Provides a limited emulation of the Configuration manager for .NET Core
 /// </summary>
 namespace System.Configuration
 {
@@ -64,5 +25,53 @@ namespace System.Configuration
         }
 
         static ConfigurationAdapter configurationProvider;
+    }
+}
+
+
+namespace TodoListDaemonWithCert
+{
+    /// <summary>
+    /// In .NET Core 1.2, provides Close() for X509Store 
+    /// </summary>
+    public static class Extension
+    {
+        public static void Close(this X509Store store)
+        {
+            store.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Simple implementation of the configuration adapter
+    /// </summary>
+    public class ConfigurationAdapter
+    {
+        public string this[string config]
+        {
+            get
+            {
+                string value;
+                var elements = xmlDocument.GetElementsByTagName("add").OfType<XmlElement>();
+                XmlElement element = elements.FirstOrDefault(e => e.Attributes.OfType<XmlAttribute>().Any(a => a.Name == "key" && a.Value == config));
+                if (element != null)
+                {
+                    value = element.Attributes.OfType<XmlAttribute>().FirstOrDefault(a => a.Name == "value").Value;
+                }
+                else
+                {
+                    value = null;
+                }
+                return value;
+            }
+        }
+
+        private readonly XmlDocument xmlDocument;
+
+        public ConfigurationAdapter(string configPath)
+        {
+            xmlDocument = new XmlDocument();
+            xmlDocument.Load(File.OpenText(configPath));
+        }
     }
 }
